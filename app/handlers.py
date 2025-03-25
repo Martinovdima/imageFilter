ec76cf1b562ff4c72572e13edc923c3c7860fe90
Martinovdima <timomartin85@gmail.com>
2025-03-25 20:35:46 +0100

    add image as FILE input in handlers.py and format check JPG, JPEG, PNG, WEBP and BMP

--- app/handlers.py
+++ app/handlers.py
@@ -27,9 +27,14 @@ async def cmd_start(message: Message):
 @router.message(Command('help'))
 async def help(message: Message):
     photo = FSInputFile('image/help.jpg')
-    text = ("🎨 Этот бот может изменить твое изображение 🎨\n\n📸   Выбирай формат с помощью кнопок   📸"
-            "\n\n📂   Загружай изображение   📂\n\n💾    Скачивай результат    💾")
-    await message.answer_photo(photo=photo, caption=text)
+    text = (
+        "🎨  Этот бот может изменить твое изображение  🎨\n"
+        "📸  Выбирай формат с помощью кнопок  📸\n"
+        "📂  Загружай изображение  📂\n"
+        "💾  Скачивай результат  💾\n"
+        "📌  Я принимаю JPG, JPEG, WEBP, BMP или PNG  📌"
+    )
+    await message.answer_photo(photo=photo, caption=text, parse_mode="MarkdownV2")
 
 @router.callback_query(F.data.startswith("art_"))
 async def process_art_callback(callback: types.CallbackQuery, state: FSMContext):
@@ -44,42 +49,58 @@ async def process_art_callback(callback: types.CallbackQuery, state: FSMContext)
 
     await callback.answer()  # Закрываем callback (убираем "часики")
 
-@router.message(ImageProcessing.waiting_for_image, F.photo)
+
+@router.message(ImageProcessing.waiting_for_image, F.photo | F.document)
 async def process_image(message: types.Message, state: FSMContext):
     try:
-        print(f'📸 Поймал фото')
+        print(f'📸 Поймал изображение')
 
         from main import bot  # Локальный импорт, чтобы избежать циклических импортов
 
         async with ChatActionSender.upload_photo(bot, message.chat.id):
             user_data = await state.get_data()
             selected_art = user_data.get("selected_art")
+
             if selected_art not in ['PIXEL', 'CARTOON', 'PENCIL']:
                 text = "❗ Сначала нужно выбрать формат\n\n📸 Выбирай... 📸"
                 await message.answer(text=text, reply_markup=await kb.art_keyboard())
+                return
 
             print(f'🎨 Выбранный фильтр: {selected_art}')
 
-            photo = message.photo[-1]  # Берем фото в максимальном качестве
-            file_id = photo.file_id
             user_id = message.from_user.id
-            file_info = await bot.get_file(photo.file_id)  # Получаем информацию о файле
-            request_id = await add_image_processing_request(user_id, file_id, selected_art)
-            print(f'Фото сохранено в базе данных')
-            file_path = file_info.file_path
 
-            print(f'📂 Путь к файлу в Telegram: {file_path}')
+            # Проверяем, что отправлено: фото или документ
+            if message.photo:
+                file_id = message.photo[-1].file_id
+                file_info = await bot.get_file(file_id)
+                file_path = file_info.file_path
+                file_name = os.path.basename(file_path)
+            elif message.document:
+                file_id = message.document.file_id
+                file_info = await bot.get_file(file_id)
+                file_path = file_info.file_path
+                file_name = message.document.file_name
+
+                # Проверяем расширение файла
+                allowed_extensions = {".jpg", ".jpeg", ".png", ".webp", ".bmp"}
+                file_ext = os.path.splitext(file_name)[-1].lower()
+
+                if file_ext not in allowed_extensions:
+                    await message.answer("❌ Неверный формат! Отправь изображение в формате JPG, JPEG, WEBP, BMP или PNG.")
+                    return
+
+            request_id = await add_image_processing_request(user_id, file_id, selected_art)
+            print(f'Изображение сохранено в базе данных')
 
             # Скачиваем файл
-            file_name = os.path.basename(file_path)
             save_path = os.path.join("infotos", file_name)
             await bot.download_file(file_path, save_path)
-
             print(f'💾 Файл сохранен в: {save_path}')
 
             await message.answer(f"Вы выбрали фильтр: {selected_art}\nИзображение загружено! 🔄 Обрабатываем...")
 
-            # Передаем путь в вашу функцию обработки
+            # Применяем фильтр
             if selected_art == 'PIXEL':
                 processed_photo_path = await convert_to_pixel_openCV(save_path, file_name)
             elif selected_art == 'CARTOON':
@@ -87,20 +108,23 @@ async def process_image(message: types.Message, state: FSMContext):
             elif selected_art == 'PENCIL':
                 processed_photo_path = await convert_to_pencil_openCV(save_path, file_name)
             else:
-                await message.answer(f"Такого формата нет!")
+                await message.answer(f"❌ Такого формата нет!")
+                return
 
             print(f'✅ Обработанное изображение: {processed_photo_path}')
 
             # Отправляем обработанное изображение
             output_path = FSInputFile(processed_photo_path)
             text = "🔶 Твое изображение готово! 🔶\n\n📸 Выбирай следующий формат... 📸"
-            sent_message = await message.answer_photo(photo=output_path, caption=text, reply_markup=await kb.art_keyboard())
+            sent_message = await message.answer_photo(photo=output_path, caption=text,
+                                                      reply_markup=await kb.art_keyboard())
 
             if sent_message.photo:
                 sent_file_id = sent_message.photo[-1].file_id
                 await update_processed_image(request_id, sent_file_id)
             else:
                 print('⚠️ Ошибка: sent_message.photo = None')
+
             os.remove(save_path)
             os.remove(processed_photo_path)
 
@@ -110,9 +134,10 @@ async def process_image(message: types.Message, state: FSMContext):
     except Exception as e:
         print(f'⚠ Ошибка: {e}')
         print(traceback.format_exc())  # Выводит полную трассировку ошибки
-        await message.answer("❌ Произошла ошибка при обработке фото.")
+        await message.answer("❌ Произошла ошибка при обработке изображения.")
 
-@router.message(F.photo)
+
+@router.message(F.photo | F.document)
 async def send_foto(message: types.Message):
     text = "❗ Сначала нужно выбрать формат \n\n📸 Выбирай... 📸"
     await message.answer(text=text, reply_markup=await kb.art_keyboard())
